9e15d0a1bec6986ce489af7ef3ba537a27795192
grupogoncalves <lucas@igenergia.com.br>
2026-05-29 08:33:14 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Preencha este arquivo antes de publicar.
+window.SDR_CONFIG = {
+  apiKey: "COLE_SUA_API_KEY_AQUI",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com",
+  projectId: "SEU_PROJETO",
+  appId: "SEU_APP_ID"
+};
