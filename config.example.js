fa8c02b59ee15bc259b7f4e95509232a055d3e3b
grupogoncalves <lucas@igenergia.com.br>
2026-05-29 08:56:45 -0300

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,9 @@
+// Copie este arquivo para config.js e preencha com os dados do seu projeto.
+// Estes dados são públicos em aplicações web, mas as permissões devem ser protegidas pelas regras do banco.
+window.SDR_CONFIG = {
+  apiKey: "COLE_SUA_API_KEY_AQUI",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com",
+  projectId: "SEU_PROJETO",
+  appId: "SEU_APP_ID"
+};
---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuração do app SDR Captura IG Energia
+// Este arquivo já está conectado ao projeto informado.
+window.SDR_CONFIG = {
+  apiKey: "AIzaSyCrU0mLYHsqID37IusblOofGcyBspHmcQ0",
+  authDomain: "sdr-captura-igenergia.firebaseapp.com",
+  databaseURL: "https://sdr-captura-igenergia-default-rtdb.firebaseio.com",
+  projectId: "sdr-captura-igenergia",
+  storageBucket: "sdr-captura-igenergia.firebasestorage.app",
+  messagingSenderId: "820401598177",
+  appId: "1:820401598177:web:78369ed4717ab36ff751a0"
+};
